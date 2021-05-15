3802740bafd41f86aaac689a48cce02a25e258d9
yangfeng <yangfengzzz@hotmail.com>
2021-05-15 11:19:51 +0800

    add more simd macros

--- src/bt_scalar.rs
+++ src/bt_scalar.rs
@@ -1,10 +1,60 @@
+use std::arch::x86_64::__m128;
+
 pub type BtScalar = f32;
+const BT_LARGE_FLOAT: f32 = 1e18;
+
+pub type BtSimdFloat4 = __m128;
 
 #[macro_export]
-macro_rules! bt_large_float {
-    ()=>{
+macro_rules! bt_castf_to128i {
+    ($a:expr)=>{
         {
-            1e18
+            $a as __m128i
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bt_castf_to128d {
+    ($a:expr)=>{
+        {
+            return $a as __m128d
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bt_casti_to128f {
+    ($a:expr)=>{
+        {
+            return $a as __m128
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bt_castd_to128f {
+    ($a:expr)=>{
+        {
+            return $a as __m128
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bt_castd_to128i {
+    ($a:expr)=>{
+        {
+            return $a as __m128i
+        }
+    }
+}
+
+#[macro_export]
+macro_rules! bt_assign128 {
+    ($r0:expr, $r1:expr, $r2:expr, $r3:expr)=>{
+        {
+            __m128($r0, $r1, $r2, $r3);
         }
     }
 }
--- src/bt_vector3.rs
+++ src/bt_vector3.rs
@@ -1,19 +1,17 @@
 use crate::bt_scalar::BtScalar;
 
-use std::arch::x86_64::{_mm_shuffle_ps, _mm_set_epi32};
+use std::arch::x86_64::{_mm_shuffle_ps, _mm_set_epi32, __m128i};
 #[macro_export]
 macro_rules! bt_shuffle {
     ($x:expr,$y:expr, $z:expr, $w:expr)=>{
-        {
-            ($w << 6 | $z << 4 | $y << 2 | $x) & 0xff
-        }
+        ($w << 6 | $z << 4 | $y << 2 | $x) & 0xff
     }
 }
 
 #[macro_export]
 macro_rules! bt_pshufd_ps {
     ($_a:expr,$_mask:expr)=>{
-        {
+        unsafe {
             _mm_shuffle_ps($_a, $_a, $_mask)
         }
     }
@@ -22,25 +20,21 @@ macro_rules! bt_pshufd_ps {
 #[macro_export]
 macro_rules! bt_splat3_ps {
     ($_a:expr,$_i:expr)=>{
-        {
-            bt_pshufd_ps!($_a, bt_shuffle($_i, $_i, $_i, 3))
-        }
+        bt_pshufd_ps!($_a, bt_shuffle($_i, $_i, $_i, 3))
     }
 }
 
 #[macro_export]
 macro_rules! bt_splat_ps {
     ($_a:expr,$_i:expr)=>{
-        {
             bt_pshufd_ps!($_a, bt_shuffle($_i, $_i, $_i, $_i))
-        }
     }
 }
 
 #[macro_export]
 macro_rules! btv_3absi_mask {
     ()=>{
-        {
+        unsafe {
             _mm_set_epi32(0x00000000, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF)
         }
     }
@@ -49,7 +43,7 @@ macro_rules! btv_3absi_mask {
 #[macro_export]
 macro_rules! btv_abs_mask {
     ()=>{
-        {
+       unsafe {
             _mm_set_epi32(0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF, 0x7FFFFFFF)
         }
     }
@@ -58,17 +52,65 @@ macro_rules! btv_abs_mask {
 #[macro_export]
 macro_rules! btv_fff0_mask {
     ()=>{
-        {
+        unsafe {
             _mm_set_epi32(0x00000000, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF)
         }
     }
 }
 
 #[macro_export]
-pub fn test() {
-    unsafe {
-        let a = btv_3absi_mask!();
-    }
+macro_rules! btv3_absf_mask {
+    () => {
+        bt_casti_to128f!(btv_3absi_mask!())
+    };
+}
 
-    let b:BtScalar = 0.0;
+#[macro_export]
+macro_rules! btv_fff0f_mask {
+    () => {
+        bt_casti_to128f!(btv_fff0_mask!())
+    };
+}
+
+#[macro_export]
+macro_rules! btv_mzero_mask {
+    () => {
+        unsafe {
+            _mm_set_ps(-0.0, -0.0, -0.0, -0.0)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! v_1110 {
+    () => {
+        unsafe {
+            _mm_set_ps(0.0, 1.0, 1.0, 1.0)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! v_half {
+    () => {
+        unsafe {
+            _mm_set_ps(0.5, 0.5, 0.5, 0.5)
+        }
+    };
+}
+
+#[macro_export]
+macro_rules! v_1_5 {
+    () => {
+        unsafe {
+            _mm_set_ps(1.5, 1.5, 1.5, 1.5)
+        }
+    };
+}
+
+#[macro_export]
+pub fn test() {
+    let a = btv_3absi_mask!();
+
+    let b: BtScalar = 0.0;
 }
